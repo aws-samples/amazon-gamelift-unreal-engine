d4943dac750cc41d89290750723349040026569f
Zack Anderson <anzacha@amazon.com>
2021-09-09 16:41:58 -0700

    Simplified login function

--- lambda/GameLiftUnreal-CognitoLogin.py
+++ lambda/GameLiftUnreal-CognitoLogin.py
@@ -1,8 +1,7 @@
 import boto3
 
-# Change to your own pool and client name
-USER_POOL_NAME = 'gameliftunreal-userpool'
-USER_POOL_CLIENT_NAME = 'gameliftunreal-login-lambda-client'
+# TODO Set to your created app client id
+USER_POOL_APP_CLIENT_ID = ''
 
 client = boto3.client('cognito-idp')
 
@@ -18,19 +17,14 @@ def lambda_handler(event, context):
     response = {
         'status': 'success',
         'tokens': resp['AuthenticationResult']
-#        'id_token': resp['AuthenticationResult']['IdToken'],
-#        'access_token': resp['AuthenticationResult']['AccessToken']
     }
     return response
 
 def initiate_auth(username, password):
     try:
-        user_pool_id = get_cognito_pool_id(USER_POOL_NAME)
-        user_pool_client_id = get_cognito_user_pool_client(user_pool_id, USER_POOL_CLIENT_NAME)
-        resp = client.admin_initiate_auth(
-            UserPoolId=user_pool_id,
-            ClientId=user_pool_client_id,
-            AuthFlow='ADMIN_USER_PASSWORD_AUTH',
+        resp = client.initiate_auth(
+            ClientId=USER_POOL_APP_CLIENT_ID,
+            AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
                 'USERNAME': username,
                 'PASSWORD': password
@@ -40,19 +34,3 @@ def initiate_auth(username, password):
     except Exception as e:
         return None, "Unknown error"
     return resp, None
-
-def get_cognito_pool_id(pool_name):
-    user_pools = client.list_user_pools(MaxResults=20)['UserPools']
-    gamelift_user_pool = list(filter(lambda pool: pool['Name'] == pool_name, user_pools))
-    if len(gamelift_user_pool) < 1:
-        return None
-        #raise Exception("Cognito user pool not found")
-    return gamelift_user_pool[0]['Id']
-
-def get_cognito_user_pool_client(pool_id, client_name):
-    pool_clients = client.list_user_pool_clients(UserPoolId=pool_id)['UserPoolClients']
-    login_client_agent = list(filter(lambda pool_client: pool_client['ClientName'] == client_name, pool_clients))
-    if len(pool_clients) < 1:
-        return None
-        #raise Exception("Cognito pool agent not found")
-    return login_client_agent[0]['ClientId']
