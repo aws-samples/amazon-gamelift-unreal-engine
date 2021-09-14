e3c8cbc91fba0e41c39ff8f01443211909a59a43
Chanh Tran <85710615+tran-11@users.noreply.github.com>
2021-09-14 08:10:55 -0700

    Add files via upload

---
+++ unreal/OfflineMainMenuWidget.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OfflineMainMenuWidget.h"
+#include "Json.h"
+#include "JsonUtilities.h"
+
+
+UOfflineMainMenuWidget::UOfflineMainMenuWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+	Http = &FHttpModule::Get();
+
+	ApiGatewayEndpoint = FString::Printf(TEXT("https://da863fd2y9.execute-api.us-west-2.amazonaws.com/test"));
+	LoginURI = FString::Printf(TEXT("/login"));
+	StartSessionURI = FString::Printf(TEXT("/startsession"));
+
+}
+
+void UOfflineMainMenuWidget::OnLoginClicked() {
+	LoginRequest(user, pass);
+}
+
+void UOfflineMainMenuWidget::LoginRequest(FString usr, FString pwd) {
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
+	JsonObject->SetStringField(TEXT("username"), *FString::Printf(TEXT("%s"), *usr));
+	JsonObject->SetStringField(TEXT("password"), *FString::Printf(TEXT("%s"), *pwd));
+
+	FString JsonBody;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&JsonBody);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), JsonWriter);
+
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> LoginHttpRequest = Http->CreateRequest();
+
+	LoginHttpRequest->SetVerb("POST");
+	LoginHttpRequest->SetURL(ApiGatewayEndpoint + LoginURI);
+	LoginHttpRequest->SetHeader("Content-Type", "application/json");
+	LoginHttpRequest->SetContentAsString(JsonBody);
+	LoginHttpRequest->OnProcessRequestComplete().BindUObject(this, &UOfflineMainMenuWidget::OnLoginResponse);
+	LoginHttpRequest->ProcessRequest();
+}
+
+void UOfflineMainMenuWidget::OnLoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (bWasSuccessful) {
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
+		if (FJsonSerializer::Deserialize(Reader, JsonObject)) {
+
+			FString IdToken = JsonObject->GetObjectField("tokens")->GetStringField("IdToken");
+
+			StartSessionRequest(IdToken);
+		}
+	}
+
+}
+
+void UOfflineMainMenuWidget::StartSessionRequest(FString idt) {
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> StartSessionHttpRequest = Http->CreateRequest();
+
+	StartSessionHttpRequest->SetVerb("GET");
+	StartSessionHttpRequest->SetURL(ApiGatewayEndpoint + StartSessionURI);
+	StartSessionHttpRequest->SetHeader("Content-Type", "application/json");
+	StartSessionHttpRequest->SetHeader("Authorization", idt);
+	StartSessionHttpRequest->OnProcessRequestComplete().BindUObject(this, &UOfflineMainMenuWidget::OnStartSessionResponse);
+	StartSessionHttpRequest->ProcessRequest();
+}
+
+void UOfflineMainMenuWidget::OnStartSessionResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (bWasSuccessful) {
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
+		if (FJsonSerializer::Deserialize(Reader, JsonObject)) {
+			IpAddress = JsonObject->GetObjectField("PlayerSession")->GetStringField("IpAddress");
+			Port = JsonObject->GetObjectField("PlayerSession")->GetStringField("Port");
+
+			//Widget BP to connect to GameLift server with IpAddress
+		
+		}
+	
+	}
+}
---
+++ unreal/OfflineMainMenuWidget.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Http.h"
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OfflineMainMenuWidget.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class GAMELIFT_UE4_API UOfflineMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UOfflineMainMenuWidget(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable)
+		void OnLoginClicked();
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+		FString ApiGatewayEndpoint;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+		FString LoginURI;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+		FString StartSessionURI;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadWrite)
+		FString user;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadWrite)
+		FString pass;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly)
+		FString IpAddress;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly)
+		FString Port;
+
+private:
+	FHttpModule* Http;
+
+	void LoginRequest(FString usr, FString pwd);
+	void OnLoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	void StartSessionRequest(FString idt);
+	void OnStartSessionResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+};
