88646d4c5e61b9bf8fdde077bef5b25f105992b9
Zack Anderson <anzacha@amazon.com>
2021-09-14 15:18:46 -0700

    Changed start session function to require specific fleet id

--- lambda/GameLiftUnreal-StartGameLiftSession.py
+++ lambda/GameLiftUnreal-StartGameLiftSession.py
@@ -3,28 +3,28 @@ import boto3
 import uuid
 import datetime
 
-game_lift = boto3.client("gamelift")
+# TODO Set to your GameLift Fleet ID
+GAMELIFT_FLEET_ID = ''
+
+game_lift = boto3.client('gamelift')
 
 def lambda_handler(event, context):
     response = {}
     game_sessions = []
     game_session = {}
-    # Getting Fleet ID 
-    fleets = game_lift.list_fleets()
-    fleet_id = fleets['FleetIds'][0]
     # Searches for a Game Session on the Fleet
-    game_sessions = game_lift.search_game_sessions(FleetId = fleet_id, FilterExpression = "hasAvailablePlayerSessions=true")
-    if(len(game_sessions['GameSessions']) == 0):
-        game_session = game_lift.create_game_session(FleetId = fleet_id, MaximumPlayerSessionCount = 80)
+    game_sessions = game_lift.search_game_sessions(FleetId = GAMELIFT_FLEET_ID, FilterExpression = 'hasAvailablePlayerSessions=true')
+    if (len(game_sessions['GameSessions']) == 0):
+        game_session = game_lift.create_game_session(FleetId = GAMELIFT_FLEET_ID, MaximumPlayerSessionCount = 80)
     else:
         game_session = game_sessions['GameSessions'][0]
-    if(game_session is not None):
+    if (game_session is not None):
         player_session = game_lift.create_player_session(GameSessionId =  game_session['GameSessionId'], PlayerId = str(uuid.uuid4()))
         response = player_session
     else:
         return {
             'statusCode': 500,
-            'body': json.dumps('Unable to find game session, check GameLift API status')
+            'body': json.dumps("Unable to find game session, check GameLift API status")
         }
     return json.dumps(response, default = myconverter).encode('UTF-8')
 def myconverter(o):
